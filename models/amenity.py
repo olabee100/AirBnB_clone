4e0f7f661bea658dfbc7066bd1b706405e378e23
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-11-14 21:19:07 +0100

    amenity class

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Defines the Amenity class
+'''
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''Amenity for Hbnb application
+    Attributes:
+        name (str): name of the Amenity
+    '''
+
+    name = ''
